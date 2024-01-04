27eacf180cc4060e2a32dd035e8da300ae022c1d
Le Van Tien <lvt030501@gmail.com>
2024-01-04 18:59:23 +0700

    Basic vue3

---
+++ my-project/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is header component</h1>
+</template>
+
+<script>
+export default {
+  name: 'HeaderVue'
+}
+</script>
+
+<style></style>
---
+++ my-project/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'popper.js';
+import 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./assets/styles/global.css"
+
+createApp(App).mount('#app')
